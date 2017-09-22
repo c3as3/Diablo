9b815d28ec3db1b09a86fb991f32d304f973cf64
Cease <c3as3@users.noreply.github.com>
2017-09-22 12:59:37 -0400

    Controller and routes update
    -created about controller and route
    -created classes controller and route
    -created guides controller and route
    -created items controller and route
    -created leaderboards controllers and route
    -created news controllers  and routes
    routed homepage to /
    -updated links on layout

--- MVC/controllers/welcome.js
+++ MVC/controllers/welcome.js
@@ -7,3 +7,31 @@ module.exports.homepage = function(req, res){
   res.render('index');
   console.log('GET Homepage')
 };
+module.exports.about = function(req, res){
+  res.render('about');
+  console.log('About Page Loaded...')
+};
+module.exports.classes = function(req, res){
+  res.render('classes');
+  console.log('Classes Page Loaded')
+};
+module.exports.guides = function(req, res){
+  res.render('guides');
+  console.log('Guides Page Loaded')
+};
+module.exports.items = function(req, res){
+  res.render('items');
+  console.log('Items Page Loaded')
+};
+module.exports.leaderboards = function(req, res){
+  res.render('leaderboards');
+  console.log('Leaderboards Page Loaded')
+};
+module.exports.news = function(req, res){
+  res.render('news');
+  console.log('News Page Loaded')
+};
+module.exports.twitch = function(req, res){
+  res.render('twitch');
+  console.log('Twitch Page Loaded')
+};
